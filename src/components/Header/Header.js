e8167d4055329b825504c2c43d850c3987f6c9dc
Steven Bull <stevenwbulljr@gmail.com>
2019-10-17 18:16:03 +0000

    complete initial header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
-  
+  const [ clicked, setClicked ] = useState(false);
+
+  const handleClick = () => {
+    setClicked(!clicked);
+  }
+
   return (
     <>
       <div>
         <h2>Restaurant Roulette</h2>
       </div>
+      { !clicked && 
       <div>
         <h4>Welcome!</h4>
-      </div>
+        <div>
+          <Link to='/register'><span onClick={handleClick}>Sign Up</span></Link>
+          <span> | </span>
+          <Link to='/login'><span onClick={handleClick}>Sign In</span></Link>
+        </div>
+      </div>}
+      { clicked && 
+      <div>
+        <Link to='/'>
+          <span onClick={handleClick}>Logout</span>
+        </Link>
+      </div>}
     </>
   );
 }
