1b4d8d207d48a67b45affca90bdaafafc6e5172d
Steven Bull <stevenwbulljr@gmail.com>
2019-10-22 16:41:12 +0000

    finish initial styling of addrestaurant form

--- src/components/AddRestaurant/AddRestaurant.js
+++ src/components/AddRestaurant/AddRestaurant.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from 'react';
 import ApiContext from '../../contexts/ApiContext';
 import { useForm } from '../../hooks/useForm';
+import './AddRestaurant.css';
 
 export default function AddRestaurant() {
   const { addRestaurants } = useContext(ApiContext);
@@ -23,55 +24,60 @@ export default function AddRestaurant() {
   const [ values, handleChange, handleSubmit ] = useForm(initialState, addNewRestaurant);
 
   return (
-    <div>
+    <div className='addRestaurantContainer'>
       <form onSubmit={handleSubmit}>
         <div>
-          <h2>Add New Restaurant</h2>
+          <h2>New Restaurant</h2>
         </div>
         <div>
-          <label htmlFor='add_restaurant_name'>Restaurant Name: </label>
-          <input 
+          <input
+            placeholder='Restaurant Name'
             type="text" 
             id='add_restaurant_name'
             name='restaurant_name'
             value={values.restaurant_name}
-            onChange={handleChange} />
+            onChange={handleChange}
+            required />
         </div>
         <div>
-          <label htmlFor='add_restaurant_street_address'>Street Address: </label>
           <input 
+            placeholder='Address'
             type="text" 
             id='add_restaurant_street_address'
             name='street_address'
             value={values.street_address}
-            onChange={handleChange} />
+            onChange={handleChange}
+            required />
         </div>
         <div>
-          <label htmlFor='add_restaurant_state_address'>City/State: </label>
           <input 
+            placeholder='City, State'
             type="text" 
             id='add_restaurant_state_address'
             name='state_address'
             value={values.state_address}
-            onChange={handleChange} />
+            onChange={handleChange}
+            required />
         </div>
         <div>
-          <label htmlFor='add_restaurant_zipcode'>Zipcode: </label>
           <input 
+            placeholder='Zipcode'
             type="integer" 
             id='add_restaurant_zipcode'
             name='zipcode'
             value={values.zipcode}
-            onChange={handleChange} />
+            onChange={handleChange}
+            required />
         </div>
         <div>
-          <label htmlFor='add_restaurant_cuisine_type'>Cuisine Type: </label>
           <input 
+            placeholder='Cuisine Type ie. American, Italian'
             type="text" 
             id='add_restaurant_cuisine_type'
             name='cuisine_type'
             value={values.cuisine_type}
-            onChange={handleChange} />
+            onChange={handleChange}
+            required />
         </div>
         {error && <span>{error}</span>}
         <div>
