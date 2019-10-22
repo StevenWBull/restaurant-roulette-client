13b2b96631a00722e09989328a8a4a14f720121b
Steven Bull <stevenwbulljr@gmail.com>
2019-10-22 14:41:26 +0000

    finish styling mobile first main page

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
+import './MainPage.css';
 
 export default function MainPage() {
   const [ register, setRegister ] = useState(false);
@@ -14,15 +15,31 @@ export default function MainPage() {
   }
 
   return (
-    <section>
+    <section className='landingPage'>
       { register && <Redirect to='/register' />}
       { signIn && <Redirect to='/login' />}
-      <div>
-        <h1>Welcome to Restaurant Roulette!</h1>
+      <div className='landingPageHeader'>
+        <div>
+          <h1>Welcome!</h1>
+        </div>
+        <div>
+          <button className='landingPageButton' onClick={handleRegister}>Sign Up</button>
+          <button className='landingPageButton' onClick={handleSignIn}>Sign In</button>
+        </div>
       </div>
-      <div>
-        <button onClick={handleRegister}>Sign Up</button>
-        <button onClick={handleSignIn}>Sign In</button>
+      <div className='landingPageDescriptions'>
+        <div className='descBox'>
+          <h3>What makes RR special?</h3>
+          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
+        </div>
+        <div className='descBox'>
+          <h3>Why choose RR?</h3>
+          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
+        </div>
+        <div className='descBox'>
+          <h3>Save your restaurants!</h3>
+          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
+        </div>
       </div>
     </section>
   )
