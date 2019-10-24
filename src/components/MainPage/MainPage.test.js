ccf9d7647403f5fa35fa889cac90454486abadb8
Steven Bull <stevenwbulljr@gmail.com>
2019-10-24 19:09:27 +0000

    finish client smoke testing

--- src/components/MainPage/MainPage.test.js
+++ src/components/MainPage/MainPage.test.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import ValidationContext from '../../contexts/ValidationContext';
 import MainPage from './MainPage';
 
 it('renders without crashing', () => {
+  const [ onlineUser, setOnlineUser ] = [ false, null ];
+
   const div = document.createElement('div')
   ReactDOM.render(
     <BrowserRouter>
-      <MainPage />
+      <ValidationContext.Provider value={[onlineUser, setOnlineUser]}>
+        <MainPage />
+      </ValidationContext.Provider>
     </BrowserRouter>,
     div
   )
