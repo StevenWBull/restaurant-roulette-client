1f2ef4d9eea10d7b48010bae3018047326ff2094
Steven Bull <stevenwbulljr@gmail.com>
2019-10-24 15:26:06 +0000

    add initial viewing modal to app

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -20,11 +20,42 @@ export default function MainPage() {
     setSignIn(true);
   }
 
+  const modalWindow = () => {
+    return (
+      <div id="myModal" className="modal">
+        <div className="modal-content">
+          <div className="modal-header">
+            <span className="close">&times;</span>
+            <h2 className='modal-header-title'>Welcome to Restaurant Roullette!</h2>
+          </div>
+          <div className="modal-body">
+            <p>Hello! Restaurant Roullette was created to simplify the saving of favorite restaurants and to give people the power to randomly generate a restaurant out of that list!</p>
+            <br />
+            <p>Just think, no more:</p>
+            <p>"Where do you want to eat, honey?" </p>
+            <p>"I don't know, where do you want to eat?"</p>
+            <br />
+            <p>With just the click of a button, you will be able to answer that question with ease!</p>
+            <br/>
+            <p>To get started, either click out of this window to create an account and get started, or click the demo button to be logged into a community account to try out the app first!</p>
+            <br /> 
+            <p>If you have any questions or suggestions, please feel free to navigate to the bottom of the page and get in contact with me.</p>
+            <br />
+            <p>Thank you and happy eating!</p>
+          </div>
+          <div className='modal-footer'>
+            <button>Demo</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <section className='landingPage'>
       { register && <Redirect to='/register' />}
       { signIn && <Redirect to='/login' />}
-      { userVisited && <div>You've visited!</div>}
+      { !userVisited && modalWindow()}
       <div className='landingPageHeader'>
         <div className='welcomeBox'>
           <div>
