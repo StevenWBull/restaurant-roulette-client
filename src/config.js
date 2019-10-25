a1e3048f46b1e50f42fa8e5053087f0d3f240e50
Steven Bull <stevenwbulljr@gmail.com>
2019-10-25 16:10:37 +0000

    finish deployment refactoring

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
   API_ENDPOINT: 'https://restaurant-roulette-server.herokuapp.com/api',
   TOKEN_KEY: 'Restaurant-Roulette',
-  DEMO_LOGIN: process.env.REACT_APP_DEMO_USER
+  DEMO_LOGIN: {user_name: process.env.REACT_APP_DEMO_USER, password: process.env.REACT_APP_DEMO_PASSWORD}
 }
