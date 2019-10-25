27ccdcf11eca5d550361bab88ddd0145a02ea9bc
Steven Bull <stevenwbulljr@gmail.com>
2019-10-25 15:33:20 +0000

    refactor code for deployment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
   API_ENDPOINT: 'https://git.heroku.com/restaurant-roulette-server.git/api',
   TOKEN_KEY: 'Restaurant-Roulette',
-  DEMO_LOGIN: {user_name: 'dunder', password: 'password'}
+  DEMO_LOGIN: process.env.REACT_APP_DEMO_USER
 }
